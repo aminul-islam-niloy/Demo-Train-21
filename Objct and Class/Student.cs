7bdb6b9559bda08f5f141fe3de07be0906d83785
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-11-25 01:27:41 +0600

    Add project files.

---
+++ Objct and Class/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Code_21.Objct_and_Class
+{
+    internal class Student
+    {
+  
+
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public  string Department { get; set; } 
+        public string Session { get; set; }
+        public string Address { get; set; }
+
+        private string password;
+        private string copon = "hhhhh";
+
+        public string getCopun()
+        {
+            return copon;
+        }
+
+        public void setPassword(string pass)
+        {
+            password = pass;
+        }
+
+        public string getpassword()
+        {
+            return password;
+        }
+
+        //public Student()
+        //{
+        //    StudentId = 1001;
+        //    Console.WriteLine("Value is setted in Constructor and it's "+ StudentId);
+        //    //note that constructor is called automatic and 
+        //    //getting and setting value is priority than initiation in constructor
+        //}
+
+        public void DisplayStudentInfo()
+        {
+            Console.WriteLine("Student Id = " + StudentId);
+            Console.WriteLine("Student Name = " + Name);
+            Console.WriteLine("Student Phone = " + Phone);
+            Console.WriteLine("Student Department = " + Department);
+            Console.WriteLine("Student Session = " + Session);
+            Console.WriteLine("Student Address = " + Address);
+            Console.WriteLine("\n");
+        }
+
+      
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo_Code_21.Objct_and_Class;
+
+namespace Demo_Code_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // OOP Class and object 
+            Student student = new Student();
+            student.Name = "Aminul";
+            student.StudentId = 1354576;
+            student.Phone = 9876543;
+            student.Department = "CSE";
+            student.Session = "2018";
+            student.Address = "Dhaka";
+
+            Student student1 = new Student();
+            student.DisplayStudentInfo();
+
+            student1 = student;
+            student1.DisplayStudentInfo();
+
+            //set and get private value
+            student.setPassword("123547");
+            string pass = student.getpassword();
+            Console.WriteLine("Password is = " + pass);
+            Console.WriteLine("Copon is : " + student.getCopun());
+
+
+            Console.ReadKey();
+        }
+    }
+}
