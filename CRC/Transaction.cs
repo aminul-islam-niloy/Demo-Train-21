2438ac2a1fca5aef035273b8e05f77e51a2635d8
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-11-26 02:48:21 +0600

    Transction is added

---
+++ CRC/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Code_21.CRC
+{
+    public class Transaction
+    {
+        // Responsibilities:
+        // 1. Record details of a financial transaction
+        // 2. Possibly update the balances of involved BankAccounts
+
+        // Collaborators:
+        // - BankAccount (for updating balances)
+
+        public void RecordTransaction(BankAccount source, BankAccount destination, decimal amount)
+        {
+
+            Console.WriteLine($"Transaction recorded: ${amount} transferred from {source} to {destination}");
+        }
+    }
+}
