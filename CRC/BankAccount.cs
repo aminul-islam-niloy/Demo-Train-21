f138b4d2c1e03e54dde8213c2e2bd9ce2152422b
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-11-26 02:42:48 +0600

    CRC is implemented

---
+++ CRC/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Code_21.CRC
+{
+    public class BankAccount
+    {
+        // Responsibilities:
+        // 1. Maintain account balance
+        // 2. Allow deposits and withdrawals
+
+        // Collaborators: None directly in this simple example
+        // (In a more complex system, it might collaborate with a Transaction class, for example)
+
+        private decimal balance;
+
+        public BankAccount(decimal initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Demo_Code_21.CRC;
 using Demo_Code_21.Objct_and_Class;
+
 
 namespace Demo_Code_21
 {
@@ -32,6 +34,26 @@ namespace Demo_Code_21
             string pass = student.getpassword();
             Console.WriteLine("Password is = " + pass);
             Console.WriteLine("Copon is : " + student.getCopun());
+
+
+            //CRC maintain
+
+            BankAccount bc = new BankAccount(1000);
+
+            Console.WriteLine( bc.GetBalance());
+
+            bc.Deposit(5000);
+            Console.WriteLine(bc.GetBalance());
+
+            bc.Withdraw(1000);
+            Console.WriteLine(bc.GetBalance());
+
+            bc.Withdraw(100000);
+            Console.WriteLine(bc.GetBalance());
+
+
+
+
 
 
             Console.ReadKey();
